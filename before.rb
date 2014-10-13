3a6c04003aa11ea8491b8f06909bb380d98dc1cd
coolesting <coolesting@gmail.com>
2014-10-12 18:24:58 -0700

    merge the files

--- before.rb
+++ before.rb
@@ -6,3 +6,17 @@ before '/www/comment' do
  	view_login?
 end
 
+		
+
+# before '/file/*' do
+# 	if request.path == '/file/upload'
+# 		view_level? _var(:upload_level, :file)
+# 	end
+# end
+
+['/view/operate', '/admin/*', '/www/edit/post', '/www/comment'].each do | path |
+	before path do
+		view_login?
+# 		view_level? _var(:form_submit_level)
+	end
+end
